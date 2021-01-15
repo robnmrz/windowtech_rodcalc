4002c0798eea067f58ee8c4f1100b77895304feb
Robin <robin.merz@icloud.com>
2021-01-15 16:26:57 +0100

    added measurements and formated html template

--- app/app/endpoints/pdfgen.py
+++ app/app/endpoints/pdfgen.py
@@ -11,41 +11,60 @@ class PdfGenerator(Resource):
         data = {
             "height": "1000",
             "width": "1000",
-            "variant": "vertikal",
+            "variant": "vertical",
             "product": "F1200",
-            "xshift": null,
-            "yshift": null
+            "xshift": None,
+            "yshift": None
         }
 
-        # default values
-        HOR_F1200_MK = 422
-        VER_F1200_MK = 170
+        # default values and add offsets if not None
+        HOR_F1200_MK = 422. + float(data.xshift if data.xshift != None else 0)
+        VER_F1200_MK = 170. + float(data.yshift if data.yshift != None else 0)
 
         # basic components
-        measure_rh_a = (data.width / 2) - 123
-        measure_rh_b = data.width - 307
-        measure_rv_a = (data.height / 2) - 225
-        measure_rv_b = data.height - 409
+        measure_rh_a = (float(data.width) / 2) - 123.
+        measure_rh_b = float(data.width) - 307.
+        measure_rv_a = (float(data.height) / 2) - 225.
+        measure_rv_b = float(data.height) - 409.
 
         # optional component horizontal
-        measure_rh_no_MV = HOR_F1200_MK - 175
+        measure_rh_no_MV = HOR_F1200_MK - 175.
         # optional component horizontal and MV
-        measure_rh_MV_a = HOR_F1200_MK - 225
-        measure_rh_MV_a = (data.width / 2) - 74
+        measure_rh_MV_a = HOR_F1200_MK - 225.
+        measure_rh_MV_b = (float(data.width) / 2) - 74.
 
+        measures = []
+
+        # add basic rods
+        measures.append(measure_rh_a, measure_rh_b)
+        measures.append(measure_rv_a, measure_rv_b)
+
+        # select optional rods
+        if data.variant != "vertical" and data.height >= 1250:
+            measures.append(measure_rh_MV_a, measure_rh_MV_b)
+        elif data.variant != "vertical":
+            measures.append(measure_rh_no_MV)
+        
         rows = [
             ['nummer1', 'nummer2', 'nummer3', 'nummer4'],
             ['nummer5', 'nummer6', 'nummer7', 'nummer8'],
             ['nummer9', 'nummer10', 'nummer11', 'nummer12'],
             ['nummer13', 'nummer14', 'nummer15', 'nummer16'],
         ]
-        render = render_template('test.html', rows=rows)
-        # font_config = FontConfiguration()
+        
+        # render html file to python string
+        render = render_template('test.html', measurements=measures)
+        
+        # define html to be converted to pdf
         html = HTML(string=render)
-        css = CSS(filename='/app/app/templates/style.css') #, font_config=font_config)
+        
+        # define path to css file for pdf styling
+        css = CSS(filename='/app/app/templates/style.css')
 
+        # generate pdf from html with css
         pdf = html.write_pdf(stylesheets=[css])
 
+        # build the http response to request
         response = make_response(pdf)
         response.headers['Content-Type'] = 'application/pdf'
         response.headers['Content-Disposition'] = 'inline;filename=output.pdf'
